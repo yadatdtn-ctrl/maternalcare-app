9c2a6e9f8a47b02060114cabf877228b8ee1d75b
Yada <yada.tdtn@gmail.com>
2026-06-02 21:52:53 +0200

    Add BMI calculator module

--- bmi_module.py
+++ bmi_module.py
@@ -1,1 +1,20 @@
 # BMI Calculator Module
+def calculate_bmi(weight, height):
+    bmi = weight / (height ** 2)
+    return round(bmi, 2)
+
+def classify_bmi(bmi):
+    if bmi < 18.5:
+        return "Underweight"
+    elif bmi >= 18.5 and bmi <= 24.9:
+        return "Normal weight"
+    else:
+        return "Overweight"
+
+
+#Testing the functions
+bmi = calculate_bmi(70, 1.70)
+print(bmi)
+
+category = classify_bmi(bmi)
+print(category)
