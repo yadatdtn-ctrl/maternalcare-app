5681b9255cf4f97e7d7f8b1f8dbf8e14d43bcc4e
Yada <yada.tdtn@gmail.com>
2026-06-02 22:21:25 +0200

    Add EDD calculator module

--- edd_module.py
+++ edd_module.py
@@ -1,1 +1,22 @@
 # Expected Delivery Date Module
+from datetime import date, timedelta
+
+def calculate_edd(Imp):
+    edd = Imp + timedelta(days=280)
+    return edd
+
+def calculate_weeks(Imp):
+    edd = Imp + timedelta(days=280)
+    return edd
+
+def calculate_weeks(Imp):
+    today = date.today()
+    days_passed = (today - Imp).days
+    weeks = days_passed // 7
+    return weeks
+
+# Testing the functions
+Imp = date(2025, 9, 1)
+print(calculate_edd(Imp))
+print(calculate_weeks(Imp))
+
